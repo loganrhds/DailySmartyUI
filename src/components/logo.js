ec4627e87b7190c82677dece1feba2c6b97e6054
loganrhds <loganrhds@gmail.com>
2020-07-03 13:17:18 -0600

    Added search and recent posts components

--- src/components/logo.js
+++ src/components/logo.js
@@ -4,7 +4,7 @@ class Logo extends Component {
   render() {
     return (
       <div className="logo-main">
-        <img src="assets/ds_circle_logo.png" alt="Daily Smarty Logo"></img>
+        <img src="assets/ds_circle_logo.png" alt="Daily Smarty Logo "></img>
       </div>
     );
   }
