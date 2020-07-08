5e0ff48293554d3902271911a327efccee6ac4f0
loganrhds <loganrhds@gmail.com>
2020-07-08 12:07:28 -0600

    set up redux devtools

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import reducers from "./reducers";
 
 import thunk from "redux-thunk";
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(
+  compose(
+    (window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f)(createStore)
+  )
+);
 
 import "./style/main.scss";
 
